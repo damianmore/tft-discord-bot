2b25bd611a80e6658215a00aed55933491eda754
damianmore <dmore0126@gmail.com>
2025-11-13 15:28:13 -0800

    fixed tft data retriever functions  which were trying to get html elements that had changed in identity on riot html in website, so it was returning an empty list

--- tft_data_retriever.py
+++ tft_data_retriever.py
@@ -40,14 +40,12 @@ def get_content_tuples(content) -> List[HTMLDataTuple]:
 async def get_patch_note_data(link) -> List[HTMLDataTuple]:
     tft_page_text = None
     tft_data_list = []
-    #page_response = requests.get(link)
     async with ClientSession() as session:
         async with session.get(link) as response:
             if response.status != 200:
                 print('Failed to retrieve patch notes')
                 return []
             tft_page_text = await response.text()
-    #if page_response.status_code == 200:
     patch_html = BeautifulSoup(tft_page_text, 'html.parser')
     error = patch_html.find(class_="page-not-found_errorCode__ZNHNq")
     if error is not None:
@@ -59,16 +57,13 @@ async def get_patch_note_data(link) -> List[HTMLDataTuple]:
         elif child.get("class") and "content-border" in " ".join(child.get("class")):
             tft_data_list.extend(get_content_tuples(child))
     return tft_data_list
-    #else:
-        #print('Failed to retrieve patch notes')
-        #return []
+
 
 async def get_recent_tft_data() -> List[HTMLDataTuple]:
     tft_patch_notes_text = None
     bot_message = []
     patch_notes_url = "https://www.leagueoflegends.com/en-us/news/tags/teamfight-tactics-patch-notes/"
 
-    #response = requests.get(patch_notes_url)
     async with ClientSession() as session:
         async with session.get(patch_notes_url) as response:
             if response.status != 200:
@@ -77,30 +72,30 @@ async def get_recent_tft_data() -> List[HTMLDataTuple]:
             tft_patch_notes_text = await response.text()
 
 
-    #if response.status_code == 200:
     soup = BeautifulSoup(tft_patch_notes_text, 'html.parser')
-    articles = soup.find_all(class_="sc-985df63-0 cGQgsO sc-d043b2-0 bZMlAb sc-86f2e710-5 eFeRux action", href=True)
+    section = soup.find("section", {"data-testid": "article-card-grid"})
 
+    articles = section.find_all("a", href=True)
+    
     for article in articles:
-        children = article.find(class_="sc-ce9b75fd-0 lmZfRs")
-        title = children.get_text(strip=True)
-        if children:
+        title_tag = article.find("div", {"data-testid": "card-title"})
+        if title_tag:  
+            title = title_tag.get_text(strip=True)
             if 'Teamfight Tactics patch' in title:
                 link = article['href']
+
                 bot_message.append(HTMLDataTuple(type="patch-title", content=title))
                 bot_message.extend(await get_patch_note_data(link))
-                break
-    return bot_message
-    #else:
-        #print("Failed to retrieve patch notes.")
-        #return []
+                return bot_message
 
-async def get_recent_patch() -> str:
+    return []
+
+
+async def get_recent_patch_title() -> str:
     tft_patch_notes_text = None
     
     patch_notes_url = "https://www.leagueoflegends.com/en-us/news/tags/teamfight-tactics-patch-notes/"
 
-    #response = requests.get(patch_notes_url)
     async with ClientSession() as session:
         async with session.get(patch_notes_url) as response:
             if response.status != 200:
@@ -108,23 +103,19 @@ async def get_recent_patch() -> str:
                 return ''
             tft_patch_notes_text = await response.text()
 
-    #if response.status_code == 200:
     soup = BeautifulSoup(tft_patch_notes_text, 'html.parser')
-    articles = soup.find_all(class_="sc-985df63-0 cGQgsO sc-d043b2-0 bZMlAb sc-86f2e710-5 eFeRux action", href=True)
+    section = soup.find("section", {"data-testid": "article-card-grid"})
+    if not section:
+        return ''  
+
+    articles = section.find_all("a", href=True)
 
     for article in articles:
-        children = article.find(class_="sc-ce9b75fd-0 lmZfRs")
-        title = children.get_text(strip=True)
-        if children:
+        title_tag = article.find("div", {"data-testid": "card-title"})
+        if title_tag:
+            title = title_tag.get_text(strip=True)
             if 'Teamfight Tactics patch' in title:
-                return title
-        return ''
-    #else:
-        #return ''
+                return title  
 
-#async def get_requested_tft_data(patch_notes_url) -> List[HTMLDataTuple]:
-    #bot_message = []
-    #bot_message = get_patch_note_data(patch_notes_url)
-    #return bot_message
-
+    return ''  
 
